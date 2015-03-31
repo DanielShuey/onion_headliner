2fbbf96f6e06a6c00f46a84df40ccabd4e6a8602
Daniel Shuey <daniel.shuey@gmail.com>
2015-03-31 08:33:17 -0700

    add example result in comment

--- onion.rb
+++ onion.rb
@@ -9,3 +9,14 @@ end
 end
 
 # cat in.txt | grep -oh "\w*asdf\w*" | grep -oh "\w*asdf$*" > out.txt
+
+#Woman/Man arrested for glomming wolves
+#Woman/Man arrested for gunning christmas
+#Woman/Man arrested for rebelling birds
+#Woman/Man arrested for precancelling paddleboards
+#Woman/Man arrested for cling parachutes
+#Woman/Man arrested for entrammelling helicopters
+#Woman/Man arrested for conning typewriters
+#Woman/Man arrested for brimming ethics
+#Woman/Man arrested for dripping bees
+#Woman/Man arrested for chitchatting groups
