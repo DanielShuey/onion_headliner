38af6cab18f8e7fd33d8b281f180f8cab288a88c
Daniel Shuey <daniel.shuey@gmail.com>
2015-03-31 08:32:26 -0700

    fix up data file

--- onion.rb
+++ onion.rb
@@ -1,9 +1,11 @@
 [:noun, :adj, :verb, :adv, :subject, :collection, :ing].each do |ling|
   define_method ling do |col=0|
-    File.readlines("#{ling}.txt").sample.strip.split(' ')[col]
+    File.readlines("#{ling}.txt").sample.strip.split(' ')[col].downcase
   end
 end
 
 10.times do
-  puts "Woman/Man arrested for #{ing} #{collection.downcase}"
+  puts "Woman/Man arrested for #{ing} #{collection}"
 end
+
+# cat in.txt | grep -oh "\w*asdf\w*" | grep -oh "\w*asdf$*" > out.txt
