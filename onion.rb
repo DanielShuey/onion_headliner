cf2ae4f93906bbd3399a77eed26443918a3ab6a6
Daniel Shuey <daniel.shuey@gmail.com>
2015-03-31 19:33:55 -0700

    more sentences and dictionaries

--- onion.rb
+++ onion.rb
@@ -1,22 +1,33 @@
-[:noun, :adj, :verb, :adv, :subject, :collection, :ing].each do |ling|
+def gender
+  %w(man woman).sample
+end
+
+%w(noun adj verb adv subject collection country ing ied ed er).each do |ling|
   define_method ling do |col=0|
     File.readlines("#{ling}.txt").sample.strip.split(' ')[col].downcase
   end
 end
 
 10.times do
-  puts "Woman/Man arrested for #{ing} #{collection}"
+  sentence = eval(File.readlines("sentence.txt").sample.strip)
+  puts sentence[0].upcase + sentence[1..-1]
 end
 
-# cat in.txt | grep -oh "\w*asdf\w*" | grep -oh "\w*asdf$*" > out.txt
+# cat in.txt | grep -oh "\w*asdf\w*" | grep -oh "\w*asdf$" > out.txt
 
-#Woman/Man arrested for glomming wolves
-#Woman/Man arrested for gunning christmas
-#Woman/Man arrested for rebelling birds
-#Woman/Man arrested for precancelling paddleboards
-#Woman/Man arrested for cling parachutes
-#Woman/Man arrested for entrammelling helicopters
-#Woman/Man arrested for conning typewriters
-#Woman/Man arrested for brimming ethics
-#Woman/Man arrested for dripping bees
-#Woman/Man arrested for chitchatting groups
+#Man arrested for damming butterflies
+#North Korea declares war on East Timor
+#Senator proclaims ice-cream is too canty
+#New bill passes through Congress allowing voting to stet
+#Man arrested for disembowelling eagles
+#New study reveals antalkalies are drearier than once thought
+#Senator proclaims sailing is too corny
+#New study reveals amphorae are freakier than once thought
+#Woman arrested for whapping phobias
+#North Korea declares war on Botswana
+#Senator proclaims 'Greek is too classy'
+#Proposed bill in Albania legislature would allow philanthropy to have folklore
+#New bill passes through Congress allowing villains to zip
+#Mozambique woman fights off attacker with saucy weddings
+#Proposed bill in Micronesia legislature would allow crime to have detectives
+#Proposed bill in Brunei legislature would allow french to have pirates
